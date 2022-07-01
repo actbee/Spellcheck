97838dbebcff23d44db45fbaf73292008055196a
actbee <rainman511@outlook.com>
2022-07-01 14:09:21 -0400

    quick fixed

--- src/editor/levelbar.ts
+++ src/editor/levelbar.ts
@@ -1,3 +1,3 @@
-export const function levelbar: number = () => {
+export const levelbar = ()=> {
     console.log("Winston TODO: calculte the distance between the user's answer and the correct one.")
 }
