a62deb655fe324ec4babe8501d24ba116e8d2d76
winstoniskandar <wpiskandar304@gmail.com>
2022-07-01 20:20:49 -0700

    Update top section function

--- src/editor/levelbar.ts
+++ src/editor/levelbar.ts
@@ -1,3 +1,18 @@
 export const levelbar = ()=> {
-    console.log("Winston TODO: calculte the distance between the user's answer and the correct one.")
+   //console.log("Winston TODO: calculate the distance between the user's answer and the correct one.")
+    const distance = (userAnswer: string, correctAnswer: string) => {
+        const userAnswerArray = userAnswer.split("");
+        const correctAnswerArray = correctAnswer.split("");
+        let distance = 0;
+        for (let i = 0; i < userAnswerArray.length; i++) {
+            if (userAnswerArray[i] !== correctAnswerArray[i]) {
+                distance++;
+            }
+        }
+        return distance;
+    } 
+    
+
+
+
 }
