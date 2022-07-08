4c9fc01d924f80a332bc2cbb7300a729fefdcdc6
Hugo Fang <hugo.fang.25@dartmouth.edu>
2022-07-08 19:45:17 -0400

    some errors to be fixed :)

--- src/editor/levelbar.tsx
+++ src/editor/levelbar.tsx
@@ -1,16 +1,24 @@
-export const levelbar = ()=> {
+import { featureFunction } from "../models/feature";
+
+export const levelbar = (userFunction: featureFunction, answerFunction: featureFunction) =>  {
    //console.log("Winston TODO: calculate the distance between the user's answer and the correct one.")
-    const distance = (userAnswer: string, correctAnswer: string) => {
-        const userAnswerArray = userAnswer.split("");
-        const correctAnswerArray = correctAnswer.split("");
-        let distance = 0;
-        for (let i = 0; i < userAnswerArray.length; i++) {
-            if (userAnswerArray[i] !== correctAnswerArray[i]) {
-                distance++;
-            }
-        }
-        return distance;
-    } 
+    // const distance = (userAnswer: string, correctAnswer: string) => {
+    //     const userAnswerArray = userAnswer.split("");
+    //     const correctAnswerArray = correctAnswer.split("");
+    //     let distance = 0;
+    //     for (let i = 0; i < userAnswerArray.length; i++) {
+    //         distance += (Number(userAnswerArray[i]) - Number(correctAnswerArray[i])) ** 2
+    //     }
+    //     return distance ** 0.5; // linear distance
+    // } 
+
+    let distance = 0;
+    for (let i = 0; i < answerFunction.feature_list.length; i++) {
+        distance += (answerFunction.feature_list[i].weight - userFunction.feature_list[i].weight) ** 2;
+    }
+    return distance ** 0.5; // linear distance
+
+
     
 
 
--- src/editor/ranked-result.tsx
+++ src/editor/ranked-result.tsx
@@ -1,8 +1,31 @@
 import UI from "../image.png";
 import React from "react";
+import { featureFunction } from "../models/feature"
+import { spellname } from "../models/spellname"
+import { speedDialClasses } from "@mui/material";
+import { levelbar } from "./levelbar";
 
 export const rankedResult = () => {
     console.log("Hugo TODO: the results in Your Spell and Merlin's Spell are calculted by spell name and the feature weights user adjusted");
+    // where are user and Merlin's featureFunction stored
+    let userFunction: featureFunction, answerFunction: featureFunction, merlinFunction: featureFunction;
+    // type error here
+    let spells = new spellname[]; // all spells
+    let threshold = 30; // only spells with distance less than threshold are suggested
+
+    let userSuggested = new spellname[];
+    let merlinSuggested = new spellname[]:
+    for (let i = 0; i < spells.length; i++) {
+        // how does levelbar.tsx receive inputs
+        if (levelbar(userFunction, spells[i]) < threshold) {
+            userSuggested.insert(spells[i]);
+        }
+        if (levelbar(merlinFunction, spells[i]) < threshold) {
+            merlinSuggested.insert(spells[i])
+        }
+    }
+
+    // display images corresonding to userSuggested and merlinSuggested in the UI
     return(
         <div className = "ranked-results">
             <div className = "spell-box">
