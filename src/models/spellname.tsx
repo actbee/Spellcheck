d873ceb850179b8cdce2539d14acec2c02a5c2d7
winstoniskandar <wpiskandar304@gmail.com>
2022-07-11 12:14:12 -0700

    completed todo #2

--- src/models/spellname.tsx
+++ src/models/spellname.tsx
@@ -1,8 +1,7 @@
 import {feature, featureFunction} from "./feature"
 
 export type spellname = {
-    type: "spellname",
+    type: string,
     img: string,
     value: number,
-    function: featureFunction,
 }
