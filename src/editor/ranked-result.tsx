ac0afbdebb24c8fda78aa6145ff030966a8833aa
Hugo Fang <hugo.fang.25@dartmouth.edu>
2022-07-17 22:10:09 -0400

    mostly done

--- src/editor/ranked-result.tsx
+++ src/editor/ranked-result.tsx
@@ -5,49 +5,51 @@ import { spellname } from "../models/spellname"
 import { speedDialClasses } from "@mui/material";
 import { valueToPercent } from "@mui/base";
 import { spelllist } from "../models/spellword";
+import { feature } from "../models/feature";
+import { useRecoilValue } from "recoil";
+import { spellwords } from "../database/spellwords";
+import { spellword } from "../models/spellword"
+
 
 export const rankedResult = () => {
     console.log("Hugo TODO: the results in Your Spell and Merlin's Spell are calculted by spell name and the feature weights user adjusted");
     
-    /*
-    // TODO: get global variables userFunction and merlinFunctions (their weight vectors)
-    let userFunction: featureFunction, merlinFunction: featureFunction;
-    let spells : spellname[]; // TODO: import all spells
-    let userVal = value(userFunction), merlinVal = value(merlinFunction); // TODO: calculate value vector using spellname and user weights
-    function compareUser(a : spellname, b : spellname) {
+    
+    let userVal: number[] = useRecoilValue(userfun), merlinVal: number[] = useRecoilValue(merlinFun); // change to user and merlin's weight functions
+    let features: feature[] = []; // TODO: import featuers of current level
+    function compareUser(a : spellword, b : spellword) {
         let da = 0;
         for (var i = 0; i < userVal.length; i++) {
-            da += (userVal[i] - a.weights[i]) ** 2 // TODO: define spellnames and their weight vectors
+            da += (userVal[i] - a.value[i]) ** 2
         }        
 
         let db = 0;
         for (var i = 0; i < userVal.length; i++) {
-            da += (userVal[i] - b.weights[i]) ** 2
+            da += (userVal[i] - b.value[i]) ** 2
         }        
         return da - db;
     }
 
-    function compareMerlin(a : spellname, b : spellname) {
+    function compareMerlin(a : spellword, b : spellword) {
         let da = 0;
         for (var i = 0; i < merlinVal.length; i++) {
-            da += (merlinVal[i] - a.weights[i]) ** 2
+            da += (merlinVal[i] - a.value[i]) ** 2
         }        
 
         let db = 0;
         for (var i = 0; i < merlinVal.length; i++) {
-            da += (merlinVal[i] - b.weights[i]) ** 2 
+            da += (merlinVal[i] - b.value[i]) ** 2 
         }        
         return da - db;
     }
-    let userSuggested = spells.sort(compareUser);
-    let merlinSuggested = spells.sort(compareMerlin);
+    let userSuggested = spellwords.sort(compareUser);
+    let merlinSuggested = spellwords.sort(compareMerlin);
 
-*/
+
 
     // display images corresonding to userSuggested and merlinSuggested in the UI
     return(
 
-        /*
         <div className = "ranked-results">
             <div className = "spell-box">
              <p>Your Spell: </p>
@@ -55,8 +57,8 @@ export const rankedResult = () => {
              <img className = "word" src = {userSuggested[0].img} width = "75" height = "75" />
              <img className = "word" src = {userSuggested[1].img} width = "75" height = "75" />
              <img className = "word" src = {userSuggested[2].img} width = "75" height = "75" />
-             <img className = "word" src = {userSuggested[3].img} width = "75" height = "75" />
-             <img className = "word" src = {userSuggested[4].img} width = "75" height = "75" />
+             {/* <img className = "word" src = {userSuggested[3].img} width = "75" height = "75" />
+             <img className = "word" src = {userSuggested[4].img} width = "75" height = "75" /> */}
           
              </div>
              </div>
@@ -69,8 +71,8 @@ export const rankedResult = () => {
              </div>
              </div>
         </div>
-        */
 
+        /*
         <div className = "ranked-results">
             <div className = "spell-box">
              <p>Your Spell: </p>
@@ -91,7 +93,7 @@ export const rankedResult = () => {
              <img className = "word" src = {UI} width = "75" height = "75" />
              </div>
              </div>
-        </div>
+        </div> */
         );
-
+        
 }
